5114401ca5da499c5b1483090db9d137f2948c82
Duskofdawn <ianperry95@gmail.com>
2021-02-17 13:21:27 -0600

    Initial Commit

---
+++ background.js
@@ -0,0 +1,17 @@
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+      chrome.declarativeContent.onPageChanged.addRules([
+        {
+          conditions: [
+            new chrome.declarativeContent.PageStateMatcher({
+                pageUrl: { urlContains: 'reddit.co' },
+            }),
+            new chrome.declarativeContent.PageStateMatcher({
+                pageUrl: { urlContains: 'redd.it' },
+            })
+          ],
+          actions: [ new chrome.declarativeContent.ShowPageAction() ]
+        }
+      ]);
+    });
+  });
---
+++ popup.js
@@ -0,0 +1,51 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('clipboardvid').addEventListener('click', copyToClipboard);
+    document.getElementById('downloadvid').addEventListener('click', downloadVideo);
+});
+
+function copyToClipboard() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        var url = tabs[0].url;
+        if (! url.endsWith('.json')) {
+            url = url + ".json";
+        }
+        var JSON = $.getJSON(url, function() {
+            var response = JSON.responseJSON[0].data.children[0].data.secure_media.reddit_video.fallback_url;
+            response = response.replace("?source=fallback", "");
+            const dummy = document.createElement("textarea");
+            dummy.style.cssText = 'opacity:0; position:fixed; width:1px; height:1px; top:0; left:0;'
+            dummy.value = response;
+            document.body.appendChild(dummy);
+            dummy.focus();
+            dummy.select();
+            document.execCommand("copy");
+            dummy.remove();
+            document.getElementById('clipboardvid').innerHTML = "Copied."
+        });
+    });
+    
+    
+};
+
+function downloadVideo() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        var currentDate = new Date();
+        var downloadName =  "vreddit-" + currentDate.getFullYear()
+                        + "-" + (currentDate.getMonth())
+                        + "-" + currentDate.getDate()
+                        + "-" + currentDate.getHours()
+                        + currentDate.getMinutes()
+                        + currentDate.getSeconds()
+                        + ".mp4";
+        var url = tabs[0].url;
+        if (! url.endsWith('.json')) {
+            url = url + ".json";
+        }
+        var JSON = $.getJSON(url, function() {
+            var response = JSON.responseJSON[0].data.children[0].data.secure_media.reddit_video.fallback_url;
+            response = response.replace("?source=fallback", "");
+            document.getElementById('downloadvid').innerHTML = "Downloading...";
+            chrome.downloads.download({url: response, filename: downloadName});
+        });
+    });
+};
